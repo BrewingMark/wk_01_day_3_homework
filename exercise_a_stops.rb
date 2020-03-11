3e9d32dc790dd0775d870173c0adcf759e1539a5
Mark Nicol <marknicol1991@live.co.uk>
2020-03-11 18:55:49 +0000

    adds Glasgow Queen St to the start of stops array

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -2,6 +2,7 @@ stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow",
 "Livingston", "Haymarket" ]
 
 stops.push("Edinburgh Waverley")
+stops.unshift("Glasgow Queen St")
 p stops
 
 #1. Add "Edinburgh Waverley" to the end of the array
