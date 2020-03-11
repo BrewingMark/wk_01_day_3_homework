2cebd58d5b94b7ce558a80fd17a3cd348344cfd5
Mark Nicol <marknicol1991@live.co.uk>
2020-03-11 22:26:39 +0000

    adds a get Avirl's pet's species

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -57,6 +57,7 @@ users = {
 p users["Jonathan"][:twitter]
 p users["Erik"][:home_town]
 p users["Erik"][:lottery_numbers]
+p users["Avril"][:pets][0][:species]
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
 # 2. Get Erik's hometown
