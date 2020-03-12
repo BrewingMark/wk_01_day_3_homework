b91dea525e3b8ee18a3d9763077c2639137c44f8
Mark Nicol <marknicol1991@live.co.uk>
2020-03-12 00:04:29 +0000

    adds a for loop to print out each capital in united kingdom

--- exercise_c_uk.rb
+++ exercise_c_uk.rb
@@ -26,6 +26,11 @@ united_kingdom[3] = {
 }
 p united_kingdom
 
+for country in united_kingdom
+  p "#{country[:name]}"
+end
+
+
 # 1. Change the capital of Wales from `"Swansea"` to `"Cardiff"`.
 # 2. Create a Hash for Northern Ireland and add it to the `united_kingdom` array (The capital is Belfast, and the population is 1,811,000).
 # 3. Use a loop to print the names of all the countries in the UK.
